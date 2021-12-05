bcddea1155c58b031928dc83d93bfd6497358411
Nina Månsson <ninamansster@gmail.com>
2021-12-05 17:44:41 +0100

    testing Tailwind in Jennies example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 const App = () => {
   return (
-    <div>
-      <h1>Hello!</h1>
-      <h1>World!</h1>
+    <div className='flex justify-between bg-green-100 my-4 mx-4'>
+      <h1 className='text-green-900'>Hello!</h1>
+      <h1 className='text-green-900'>Green World!</h1>
     </div>
   )
 }
